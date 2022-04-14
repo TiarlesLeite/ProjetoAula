b2910ec30dadf3304135e5ac6b82155aa7707fce
TiarlesLeite <tiarlesleite@opensolucoes.net>
2022-04-13 23:33:41 -0300

    add  alteração

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+   int x = 10;
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+ 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
